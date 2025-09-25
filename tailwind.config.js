78dcf76476b06f6db4be7df90fc91f1d3b4c0231
whoistavs <octavio.marvel2018@gmail.com>
2025-09-25 10:52:58 -0300

    Melhora TBT

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./*.html", // Isso vai procurar as classes em todos os arquivos HTML da pasta raiz.
+    // Se você tiver arquivos JavaScript com classes, adicione-os aqui:
+    // "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
